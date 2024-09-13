8354b20612de43c503ef6b30a8ec7c09bf93eae9
Laura Berge <lauraeberge@gmail.com>
2024-09-13 09:11:50 -0500

    add blob and fetch definition for codegrade

--- src/__tests__/setup.js
+++ src/__tests__/setup.js
@@ -1,6 +1,8 @@
 import { afterEach } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/vitest'
+import 'whatwg-fetch';
+import {Blob} from 'node:buffer';
 
 afterEach(() => {
     cleanup();
